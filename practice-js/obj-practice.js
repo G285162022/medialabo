6ba19f5b9ddd6fa08353162a535d06bb9a959ecc
G285162022 <228516@st.takushoku-u.ac.jp>
2023-06-22 15:24:08 +0900

    課題

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -6,7 +6,7 @@ let campus = {
 	lon: 35.624869704425,
 	lat: 139.28201056633
 };
-console.log(campas.address);
+console.log(campus.address);
 
 let gakka = [
 	{name: "機械システム工学科", ename: "Department of Mechanical Systems Engineering"},
