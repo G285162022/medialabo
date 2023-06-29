69950bd6d00b6946f2f803f73758da0251eae17b
G285162022 <228516@st.takushoku-u.ac.jp>
2023-06-29 18:33:41 +0900

    課題

--- kazuate.js
+++ kazuate.js
@@ -4,34 +4,60 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 1;
+let yoso = 4;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+
 hantei();
-hantei();
-hantei();
-hantei();
+// hantei();
+// hantei();
+// hantei();
+
+let span_kaisu =document.querySelector("span#kaisu");
+span_kaisu.textContent=kaisu;
+let span_answer =document.querySelector("span#answer");
+span_answer.textContent=yoso;
+
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
+    let p1 =document.querySelector("p#result");
     // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-    let yoso = 4;
-    console.log(kaisu + "回目の予想:" + yoso);
-    if (kaisu === 3 && yoso !== kotae){
-        console.log("まちがい。残念でした。答えは" + kotae + "です。");
-    }else{
-        if (kaisu >= 4){
-            console.log("答えは" + kotae + "でした。すでにゲームは終わっています。");
-        }else {
-            if (yoso === kotae) {
-                console.log("正解です。おめでとう！");
-            } else if (yoso !== kotae && yoso <= kotae) {
-                console.log("まちがい。答えはもっと小さいですよ。");
-            } else if (yoso !== kotae && yoso >= kotae) {
-                console.log("まちがい。答えはもっと大きいですよ。");
-            }   
+    // if (kaisu === 3 && yoso !== kotae){
+    //     p1.textContent="まちがい。残念でした。答えは" + kotae + "です。";
+    // }else{
+    //     if (kaisu >= 4){
+    //         p1.textContent="答えは" + kotae + "でした。すでにゲームは終わっています。";
+    //     }else {
+    //         if (yoso === kotae) {
+    //             p1.textContent="正解です。おめでとう！";
+    //         } else if (yoso !== kotae && yoso <= kotae) {
+    //             p1.textContent="まちがい。答えはもっと小さいですよ。";
+    //         } else if (yoso !== kotae && yoso >= kotae) {
+    //             p1.textContent="まちがい。答えはもっと大きいですよ。";
+    //         }   
+    //     }
+    // }
+    if (kaisu < 3 ){
+        if (yoso === kotae){
+            p1.textContent = '正解です。おめでとう！';
+        }else if (yoso > kotae){
+         p1.textContent = 'まちがい。答えはもっと小さいですよ。';
+        }else{
+            p1.textContent="まちがい。答えはもっと大きいですよ。";
         }
+    }
+    if (kaisu === 3){
+        if (yoso === kotae){
+            p1.textContent="正解です。おめでとう！";
+        }else{
+            p1.textContent="まちがい。残念でした。答えは" + kotae + "です。";
+        }
+    }
+    if(kaisu > 3){
+        p1.textContent="答えは" + kotae + "でした。すでにゲームは終わっています。";
     }
     kaisu = kaisu + 1;
 }
--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,13 +14,51 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
-
+let h2 = document.querySelector('h2#ex42');
+let p = document.createElement('p');
+p.textContent='写真表と都市の緯度経度のページです';
+h2.insertAdjacentElement('afterend',p);
+p.style.textEmphasis="sesame green";
 
 // 練習4-3 写真表作成プログラム
+let div1=document.querySelector("div#phototable");
+let p1=document.createElement("p1");
+p1=document.createElement("p");
+let img1 =document.createElement("img");
+img1.setAttribute=("src");
+img1.src="taro.png";
+p1.insertAdjacentElement("beforeend",img1);
+div1.insertAdjacentElement("beforeend",p1);
+
+let div2=document.querySelector("div#phototable");
+let p2=document.createElement("p2");
+p2=document.createElement("p");
+let img2 =document.createElement("img");
+img2.setAttribute=("src");
+img2.src="jiro.png";
+p2.insertAdjacentElement("beforeend",img2);
+div2.insertAdjacentElement("beforeend",p2);
+
+let div3=document.querySelector("div#phototable");
+let p3=document.createElement("p3");
+p3=document.createElement("p");
+let img3 =document.createElement("img");
+img3.setAttribute=("src");
+img3.src="hanako.png";
+p3.insertAdjacentElement("beforeend",img3);
+div3.insertAdjacentElement("beforeend",p3);
 
 
 // 練習4-4 箇条書き削除プログラム
-
+let w = document.querySelectorAll('ul#location > li');
+for (let n of w) {
+	n.remove();
+  }
 
 // 練習4-5 箇条書き追加プログラム
-
+  let o =document.querySelector("ul#location");
+  for (let h of data){
+	let g= document.createElement("li");
+	g.textContent=h.name + "... 緯度:" + h.lat + ", 経度:" + h.lng;
+	o.insertAdjacentElement("beforeend",g);
+	}
